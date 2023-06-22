718285689529a63664bc3f11b71421060df5a448
Vlad Iftimescu <V.G.Iftimescu@student.tudelft.nl>
2023-06-22 18:35:51 +0300

    Add project files.

---
+++ Data/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChannelEngineConsoleApp
+{
+    internal class Order {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ Data/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ChannelEngineConsoleApp
+{
+    internal class Product {
+        public string Name { get; set; }
+        public string Gtin { get; set; }
+        public int Quantity { get; set; }
+    }
+}
